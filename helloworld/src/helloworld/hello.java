69f5f7615f41f4c7827ce35b4259e4e1e33c3922
seoil <seoil@HH311-33>
2017-11-23 11:18:37 +0900

    add print line

--- helloworld/src/helloworld/hello.java
+++ helloworld/src/helloworld/hello.java
@@ -5,6 +5,6 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.printf("hello world");
+			System.out.printf("hello everyone");   
 	}
-
 }
