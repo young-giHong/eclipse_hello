10436100d2c666353373ce5b88ca6699cb0e71f1
young-giHong <65478912@naver.com>
2017-11-30 11:16:15 +0900

    pushprint.java on github

--- helloworld/src/helloworld/pushprint.java
+++ helloworld/src/helloworld/pushprint.java
@@ -3,5 +3,6 @@ package helloworld;
 public class pushprint {
 	public static void main(String[] args) {
 		System.out.println("Add Java Class File Push");
+		System.out.println("world with 365");
 	}
 }
